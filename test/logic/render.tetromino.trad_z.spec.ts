2b3234c88f358f60c63e74df67e391edbf4a3d4e
David Waller <david@krawaller.se>
2018-05-22 07:04:32 +0200

    reformat trad_z test

--- test/logic/render.tetromino.trad_z.spec.ts
+++ test/logic/render.tetromino.trad_z.spec.ts
@@ -7,7 +7,7 @@ test('The renderTetromino function when called with a trad_z', t => {
   const tetr = makeTetromino('trad_z', 'bmoy', 0, [2,0]);
 
   t.deepEqual(
-    stringifyRender(renderTetromino(tetr), 5, 3),
+    stringifyRender(renderTetromino({...tetr, frameNumber: 0}), 5, 3),
     [
       '  bm ',
       '   oy',
@@ -16,10 +16,8 @@ test('The renderTetromino function when called with a trad_z', t => {
     'frame 0 is correctly infoed'
   );
 
-  tetr.frameNumber = 1;
-
   t.deepEqual(
-    stringifyRender(renderTetromino(tetr), 5, 3),
+    stringifyRender(renderTetromino({...tetr, frameNumber: 1}), 5, 3),
     [
       '    b',
       '   om',
@@ -28,10 +26,8 @@ test('The renderTetromino function when called with a trad_z', t => {
     'frame 1 is correctly infoed'
   );
 
-  tetr.frameNumber = 2;
-
   t.deepEqual(
-    stringifyRender(renderTetromino(tetr), 5, 3),
+    stringifyRender(renderTetromino({...tetr, frameNumber: 2}), 5, 3),
     [
       '     ',
       '  yo ',
@@ -40,10 +36,8 @@ test('The renderTetromino function when called with a trad_z', t => {
     'frame 2 is correctly infoed'
   );
 
-  tetr.frameNumber = 3;
-
   t.deepEqual(
-    stringifyRender(renderTetromino(tetr), 5, 3),
+    stringifyRender(renderTetromino({...tetr, frameNumber: 3}), 5, 3),
     [
       '   y ',
       '  mo ',
